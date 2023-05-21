eb5d8519b04c4e7304a014ad8bcf54f55e1fb190
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-05-22 00:12:23 +0800

    Added a statement whenever they choose an operation

--- Calculator_OOP.py
+++ Calculator_OOP.py
@@ -9,6 +9,7 @@ class Calculator:
             print("Invalid. Please try again")
 #Add a function that add the two numbers
     def add(self, num1, num2):
+        print("You chose Addition as your operation")
         try:
             sum = num1 + num2
             print("The sum of the two number is:", sum)
@@ -17,6 +18,7 @@ class Calculator:
 
 #Add a function that subtract the two numbers
     def subtract(self, num1, num2):
+        print("You chose Subtraction as your operation")
         try:
             difference = num1 -num2
             print("The difference of the two number is:", difference)
@@ -25,6 +27,7 @@ class Calculator:
 
 #Add a function that multiply the two numbers
     def multiply(self, num1, num2):
+        print("You chose Multiplication as your operation")
         try:
             product = num1*num2
             print("The product of the two number is:", product)
@@ -32,8 +35,10 @@ class Calculator:
             print("Invalid. Please try again")
 #Add a function that divide the two numbers
     def divide(self, num1, num2):
+        print("You chose Division as your operation")
         try:
             quotient = num1/num2
             print("The quotient of the two number is", quotient)
         except (ValueError, ZeroDivisionError):
             print("Invalid. Please try again")
+    
