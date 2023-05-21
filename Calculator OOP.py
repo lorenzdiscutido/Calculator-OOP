a6ed817fee22ea382afeeba6e4a9523513f351b7
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-05-21 23:18:48 +0800

    Pseudocode

---
+++ Calculator OOP.py
@@ -0,0 +1,7 @@
+#Create a class named calculator
+#Add a function that ask the user to input two numbers
+#Add a function that add the two numbers
+#Add a function that subtract the two numbers
+#Add a function that multiply the two numbers
+#Add a function that divide the two numbers
+#add a function that prints the answer
