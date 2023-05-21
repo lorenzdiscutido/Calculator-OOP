dea22e6dc924cde948c5a75fe1b80cd12ad6d751
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-05-21 23:26:21 +0800

    Added a function to ask the user to input two numbers

--- Calculator OOP.py
+++ Calculator OOP.py
@@ -1,6 +1,12 @@
 #Create a class named calculator
 class Calculator:
 #Add a function that ask the user to input two numbers
+    try:
+        def input(self):
+            num1 = float(input("Please enter your first number:"))
+            num2 = float(input("Please eneter your second number:"))
+    except ValueError:
+        print("Invalid. Please try again")
 #Add a function that add the two numbers
 #Add a function that subtract the two numbers
 #Add a function that multiply the two numbers
