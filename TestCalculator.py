16da4bd247c2c90acf4f6b6cc35268466bbb962f
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-05-22 01:16:12 +0800

    Add function is working properly

--- TestCalculator.py
+++ TestCalculator.py
@@ -11,3 +11,8 @@ while True:
     #ask the user for their numbers
     num1 = calc.input()
     num2 = calc.input()
+
+    #ask the user what operation they like to use
+    option = calc.operation()
+    if option == "1":
+        sum = calc.add(num1, num2)
