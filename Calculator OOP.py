e66e7b46431cc359fab11f254f444baca4ea4cd0
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-05-21 23:38:36 +0800

    Added a function that subtracts the two number

--- Calculator OOP.py
+++ Calculator OOP.py
@@ -13,8 +13,15 @@ class Calculator:
             sum = num1 + num2
             print("The sum of the two number is:", sum)
         except ValueError:
-            print("Invalid. Please try again")
+            print("Invalid. Please try again")  
+
 #Add a function that subtract the two numbers
+    def subtract(self, num1, num2):
+        try:
+            difference = num1 -num2
+            print("The difference of the two number is:", difference)
+        except ValueError:
+            print("Invalid. Please try again")
 #Add a function that multiply the two numbers
 #Add a function that divide the two numbers
 #add a function that prints the answer
